5ffdfc4391dc9290ca1c03266dc152238951e2ce
Andrey Kulpanovich <kulpanovich@yahoo.com>
2020-01-05 16:45:59 -0500

    test added

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,20 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void Test1()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New name");
+
+            Assert.Equal("New name", book1.Name);
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
@@ -13,6 +27,17 @@ namespace GradeBook.Tests
 
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
         Book GetBook(string name)
